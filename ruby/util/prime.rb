688788c84f3f20f9ffa758d8064fb4dce016beac
unknown <Matthew@.(none)>
2010-09-03 17:15:25 -1000

    Adding new files

--- ruby/util/prime.rb
+++ ruby/util/prime.rb
@@ -1,5 +1,31 @@
 #!/usr/bin/env ruby
 
+def sieve(max)
+	n_sq = max
+	n = Math.sqrt(n_sq)
+	a =(0..n_sq).to_a
+	a[0] = nil
+	a[1] = nil
+	index = 2
+
+	while index <= n do
+		# remove multiples of index up to n_sq
+		multiple = index * 2
+		while multiple <= n_sq do
+			a[multiple] = nil
+			multiple += index
+		end
+		
+		# get next index
+		index += 1
+		while index <=n and a[index] == nil do
+			index += 1
+		end
+	end
+	s = SortedSet.new
+	s.merge(a.compact)
+end
+
 def prime_generator()
 	primes = [2]
 	yield 2
